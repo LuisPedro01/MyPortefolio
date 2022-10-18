084d184b068f57696a1ae7c7f21667cbc57a7b26
Luis Pedro Carvalho Rodrigues <a42597@alunos.ipb.pt>
2022-10-18 23:32:54 +0100

    fix errors

--- src/pages/SuporteMe.js
+++ src/pages/SuporteMe.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import MonetizationOnTwoToneIcon from '@material-ui/icons/MonetizationOnTwoTone';
-import "../assets/AboutMe.css";
+import React from "react";
+import MonetizationOnTwoToneIcon from "@material-ui/icons/MonetizationOnTwoTone";
+import "../assets/SuportMe.css";
 
 function SuportMe() {
   return (
-    <div className='aboutme'>
-        <div className='about1'>
-            <h1>If you want to collaborate with me and give some support donate some!</h1> 
-            <div>
-              <a href='https://ko-fi.com/luisrodrigues66052' target="_blank"><MonetizationOnTwoToneIcon/></a>
-            </div>              
-        </div>        
+    <div className="suport1">
+      <h1>
+        If you want to collaborate with me and give some support donate some!
+      </h1>
+      <p>Donate to Ko-Fi</p>
+      <a href="https://ko-fi.com/luisrodrigues66052" target="_blank">
+        <MonetizationOnTwoToneIcon />
+      </a>
     </div>
-  )
+  );
 }
 
-
-export default SuportMe
+export default SuportMe;
